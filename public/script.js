08aa5e7df8283275e1f3cad563f57df269e375a1
MercutioG <110576904+MercutioG@users.noreply.github.com>
2023-10-10 15:17:24 -0700

    Set up a grid
    - Started the json to randomize cards
    - Created SCSS to style a bit

---
+++ public/script.js
@@ -0,0 +1,8 @@
+// NEEDS JQUERY DO NOT TOUCH
+// $(function () {
+//   var deck = $("#deck");
+//   var cards = deck.children();
+//   while (cards.length) {
+//       deck.append(cards.splice(Math.floor(Math.random() * cards.length), 1)[0]);
+//   }
+// });
