f23b6ad73796a098e57b6a39360659c7f81561c9
MercutioG <110576904+MercutioG@users.noreply.github.com>
2023-10-17 15:23:29 -0700

    Finished all the functionality
    - Still working on win screen
    - Mongo db and login not complete

--- public/script.js
+++ public/script.js
@@ -1,7 +1,11 @@
 var selection = 0;
 var playerTurn = 1;
-var playerLimit = 2;
+var playerLimit = 3;
 var timeBonus = 50;
+
+if(sessionStorage.getItem('setPlayerTurns') > 0){
+  playerLimit = sessionStorage.getItem('setPlayerTurns')
+}
 
 var playerScores = [];
 for(var i = 0; i < playerLimit; i++) {
@@ -13,10 +17,19 @@ var card1id;
 var card2;
 var card2id;
 
-setInterval(() => {
+var pause = false;
+
+function setPlayerCount(playerCount){
+  sessionStorage.setItem('setPlayerTurns', playerCount)
+  location.reload()
+}
+
+var timerScore = setInterval(() => {
   if(timeBonus > 0){
-    timeBonus--
-    document.getElementById('time-bonus-display').innerHTML = timeBonus;
+    if(!pause){
+      timeBonus--
+      document.getElementById('time-bonus-display').innerHTML = timeBonus;
+    }
   }
 }, 1000)
 
@@ -28,37 +41,47 @@ $(window).on('load', function(){
   //   deck.append(cards.splice(Math.floor(Math.random() * cards.length), 1)[0]);
   // }
 
+  $('#options-menu').on('click', function() {
+    $('#options').toggleClass('options-on')
+    pause = !pause;
+  })
+  // When a player selects a non-selected card
   $('.playing-card').on('click', function(){
-    if(selection == 0) {
+    if(selection == 0 && !pause) {
       // Card Selection 1
       $(this).removeClass('playing-card')
       card1 = this.className
       card1id= this.id
-      console.log('card 1 is ' + card1)
       selection++
-      $(this).addClass('limbo-card').addClass('flipped')
-      console.log(selection);
-    } else if (selection == 1 && card1id != this.id) {
+      $(this).addClass('limbo-card')
+    } else if (selection == 1 && card1id != this.id && !pause) {
       // Card Selection 2
+      $(this).removeClass('playing-card')
       card2 = this.className
       selection++
-      console.log(selection)
-      $(this).addClass('limbo-card').addClass('flipped')
-      checkCards()
+      $(this).addClass('limbo-card')
+      
+      setTimeout(checkCards, 100)
     }
 
     function checkCards(){
       // Check if Cards Match
       if(card1 == card2){
         playerScores[playerTurn-1] += 50 + timeBonus
-        console.log(playerScores + ' is the score')
-        timeBonus = 50
-        $('.limbo-card').addClass('claimed-card').removeClass('limbo-card')
+        document.getElementById('player-score').innerHTML = ('Player Scores: ' + playerScores.join(', '))
+        $('.limbo-card').addClass('claimed-card').removeClass('limbo-card').off()
       } else {
-        $('.limbo-card').addClass('playing-card').removeClass('limbo-card').removeClass('flipped')
+        $('.limbo-card').addClass('playing-card').removeClass('limbo-card')
       }
-      
+      // Reset turn and pass to the next player
+      timeBonus = 50
+      selection = 0
       if(playerTurn >= playerLimit) {playerTurn = 1} else {playerTurn++}
+      document.getElementById('player-turn').innerHTML = 'Player ' + playerTurn + "'s Turn"
+      if(document.getElementsByClassName('playing-card').length < 1){
+        console.log('Someone won')
+        clearInterval(timerScore)
+      }
     }
   })
 })
